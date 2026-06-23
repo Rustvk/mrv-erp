952c7a97170d4750d1ffe42dd16f11b89d2571e2
Rustam Mambetov <rustvk@gmail.com>
2026-06-23 14:18:29 +0500

    fix(infrastructure): фикс импорта стилей и доступа к домену сторибука (#14)

--- apps/finance/tailwind.config.js
+++ apps/finance/tailwind.config.js
@@ -1,9 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [require('@mrv-erp/config-tailwind')],
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    // Если появится packages/ui
-    // "../../packages/ui/src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', '../../packages/ui/src/**/*.{js,ts,jsx,tsx}'],
 };
--- apps/supply/src/index.tsx
+++ apps/supply/src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
-// import './app/styles/index.css';
+import './app/styles/index.css';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Root element not found');
